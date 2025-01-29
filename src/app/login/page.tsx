bf1f65465fe3db6f320a90bd25d3637759e3e313
Jandel Macabecha <jandel.macabecha88@gmail.com>
2025-01-29 17:02:03 +0800

    landing page implementation

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='border-2 border-green-500'>
+      test
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,27 @@
-import Image from "next/image";
+import logo from '../../public/images/logo.png'
+import star from '../../public/images/star.png'
+import squiggly from '../../public/images/squiggly.png'
+import meeting from '../../public/images/meeting.png'
+import { Button, Typography } from '@mui/material';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="h-screen flex flex-col relative overflow-hidden">
+      <div className="z-0 absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:3rem_3rem]">
+        <img style={{position:'absolute', height:'8dvw', transform:'translate(-50%,-50%)', top:'20%', left:'50%'}} src={star.src} alt="star" />
+        <img style={{position:'absolute', height:'24dvw', transform:'translate(0%,-50%)', top:'88%', left:'0%'}} src={squiggly.src} alt="star" />
+        <img style={{position:'absolute', height:'50dvw', transform:'translate(-50%,20%)', bottom:'10%', left:'70%'}} src={meeting.src} alt="star" />
+      </div>
+      <div className="p-10 flex justify-between items-center relative" style={{zIndex:1}}>
+        <img src={logo.src} alt="logo" style={{height:'8dvh'}} />
+        <span className='flex-grow flex flex-col md:flex-row lg:flex-row xl:flex-row items-center gap-2 justify-end font-bold' style={{fontSize:'clamp(0.8em, 0.5dvw + 0.8em, 1em)'}}>Don't have an account? <a href='/' style={{color:'#7D57FC', textDecoration:'underline'}}>Register Now!</a></span>
+      </div>
+      <div className='relative w-screen md:w-1/2 lg:w-1/2 xl:w-1/2 p-5'>
+        <Typography sx={{fontSize:'clamp(2em, 7dvw + 2em, 5.5em)'}} fontWeight='bold'>Organize Your Workflow</Typography>
+        <Typography sx={{fontSize:'clamp(1em, 0.3dvw + 1em, 1.5em)', width:'70%'}}>Simplify project progress tracking with streamlined and easified queueing management.</Typography>
+        <Button sx={{backgroundColor:'#CCFC57', border:'solid 1px black', color:'black', borderRadius:'15px', padding:'0.5em 3em', marginTop:'40px'}}>Login</Button>
+      </div>
     </div>
   );
 }
