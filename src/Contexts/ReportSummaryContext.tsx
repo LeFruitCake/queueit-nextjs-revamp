5a4239de1eb3996d21c5beeffb44aa4274fb7839
Jandel Macabecha <jandel.macabecha88@gmail.com>
2025-03-06 18:25:11 +0800

    grade summary implementation, unstable

---
+++ src/Contexts/ReportSummaryContext.tsx
@@ -0,0 +1,46 @@
+"use client"
+import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { ReportSummary } from '../Utils/Global_variables';
+
+// Define the context type
+interface ReportSummaryContextType {
+  ReportSummary: ReportSummary | undefined;
+  setReportSummary: (ReportSummary: ReportSummary | undefined) => void;
+}
+
+// Create the context
+const ReportSummaryContext = createContext<ReportSummaryContextType | undefined>(undefined);
+
+// Create a provider component
+export const ReportSummaryProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [ReportSummary, setReportSummary] = useState<ReportSummary | undefined>(() => {
+    // Retrieve ReportSummary from localStorage if available
+    const storedReportSummary = localStorage.getItem('ReportSummary');
+    return storedReportSummary ? JSON.parse(storedReportSummary) : undefined;
+  });
+
+  useEffect(() => {
+    // Store ReportSummary in localStorage whenever it changes
+    if (ReportSummary) {
+      // const ReportSummaryWithMembers = {...ReportSummary, members:Array.from(ReportSummary.members)}
+      localStorage.setItem('ReportSummary', JSON.stringify(ReportSummary));
+    } else {
+      localStorage.removeItem('ReportSummary'); // Clear if undefined
+    }
+  }, [ReportSummary]);
+
+  return (
+    <ReportSummaryContext.Provider value={{ ReportSummary, setReportSummary }}>
+      {children}
+    </ReportSummaryContext.Provider>
+  );
+};
+
+// Create a custom hook to use the ReportSummaryContext
+export const useReportSummaryContext = () => {
+  const context = useContext(ReportSummaryContext);
+  if (context === undefined) {
+    throw new Error('ReportSummary Context must be used within a ReportSummaryProvider');
+  }
+  return context;
+};
