7379cd2f0f50b60cced0899dde22a803fbced814
Jandel Macabecha <jandel.macabecha88@gmail.com>
2025-03-17 11:47:51 +0800

    Notification for defaulted and started automated meetings

--- src/Components/Notification.tsx
+++ src/Components/Notification.tsx
@@ -10,7 +10,9 @@ interface NotificationProps{
 const Notification:React.FC<NotificationProps> = ({notification}) => {
     const  avatar = useRef(notification.notification.notificationType === NotificationType.TEAM_ENQUEUE?randomGroupImage():randomAvatar())
     return (
-        <div className={`flex relative border-2 p-3 shadow-md ${notification.notification.notificationType === NotificationType.APPOINTMENT_CANCELLED || notification.notification.notificationType === NotificationType.QUEUEING_CLOSE?'shadow-red-300':'shadow-green-300' }`}>
+        <div className={`flex relative border-2 p-3 shadow-md ${notification.notification.notificationType === NotificationType.APPOINTMENT_CANCELLED || notification.notification.notificationType === NotificationType.QUEUEING_CLOSE || notification.notification.notificationType === NotificationType.APPOINTMENT_DEFAULTED?'shadow-red-300':
+            notification.notification.notificationType === NotificationType.AUTOMATED_APPOINTMENT_STARTED?'shadow-blue-300':'shadow-green-300'
+         }`}>
                 <img src={avatar.current} alt="avatar" style={{height:'15%', width:'15%'}}/>
             <div className='flex-1 flex flex-col justify-center'>
                 <Typography variant='subtitle2' fontWeight={"bold"}>{notification.notification.notificationMessage}</Typography>
